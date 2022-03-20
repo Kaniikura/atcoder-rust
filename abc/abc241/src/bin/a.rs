d733ad7f7f1087b0e0e56504f34815146b1f5259
Kaniikura <constantl492@gmail.com>
2022-03-20 23:16:48 +0900

    AC

--- abc/abc241/src/bin/a.rs
+++ abc/abc241/src/bin/a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: [usize; 10],
+    }
+
+    let num_push: usize = 3;
+    let mut cur: usize = 0;
+    for _ in 0..num_push {
+        cur = a[cur];
+    }
+    println!("{}", cur);
 }
--- abc/abc241/src/bin/b.rs
+++ abc/abc241/src/bin/b.rs
@@ -1,3 +1,39 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        m: usize,
+        mut a: [usize; n],
+        b: [usize; m],
+    }
+
+    let mut idx: usize = 0;
+    let mut is_found: bool = false;
+    let mut ans: bool = true;
+
+    for b_i in b.iter() {
+        for (j, a_j) in a.iter().enumerate() {
+            // 見つけた！
+            if b_i == a_j {
+                idx = j;
+                is_found = true
+            }
+        }
+        // 食べたい長さのパスタのなかった時点で終了
+        if !is_found {
+            ans = false;
+            break;
+        // パスタがあったら食べて、次の日に
+        } else {
+            a[idx] = 0; // 食べる
+            is_found = false;
+        }
+    }
+
+    if ans {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
 }
